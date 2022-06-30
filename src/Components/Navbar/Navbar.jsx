8c9c4fce12fca9a6c2836a42f3a88ecf0be65bf9
vishwakarma-prashant <102661490+vishwakarma-prashant@users.noreply.github.com>
2022-06-30 14:32:22 +0530

    router

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,24 +4,32 @@ import logo from "../assest/logoimg.png";
 import ShoppingCart from "@mui/icons-material/ShoppingCart";
 import Badge from "@mui/material/Badge";
 import MailIcon from "@mui/icons-material/Mail";
+import {Link ,useLocation} from "react-router-dom"
 import "./styles.css"
 
 const Navbar = ({totalItem}) => {
+  const location =useLocation();
   return (
     <>
       <AppBar position="fixed" color="inherit" className="appbar">
         <Toolbar className="toolbar">
-          <Typography variant="h6" color="inherit" className="title">
+          <Typography variant="h6" color="inherit" className="title" component={Link} to="/" >
+       
+
             <img src={logo} height="30px" alt=""  className="imgLogo"/>
              E commerce
+           
           </Typography>
           <div className="grow"/>
           <div className="button">
+            { location.pathname ==="/" ?  (
+            <Link to="/cart">
             <IconButton>
               <Badge badgeContent={totalItem} color="primary">
                 <ShoppingCart />
               </Badge>
             </IconButton>
+            </Link>) : null}
           </div>
         </Toolbar>
       </AppBar>
